6a428f7f057ad5134c3895cb2ade11abac6b71c0
zeroheight <tokens-management@zeroheight.com>
2025-02-28 11:15:19 +1030

    Update Andrews DS testing from zeroheight

---
+++ js/token_Primitives_Mode1.js
@@ -0,0 +1,478 @@
+module.exports = {
+  "color": {
+    "colour": {
+      "blue": {
+        "100": {
+          "value": "#130064",
+          "original": {
+            "value": "#130064"
+          },
+          "name": "ColorColourBlue100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "blue",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "blue",
+            "100"
+          ]
+        }
+      },
+      "green": {
+        "10": {
+          "value": "#e6f1f0",
+          "original": {
+            "value": "#e6f1f0"
+          },
+          "name": "ColorColourGreen10",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "green",
+            "subitem": "10"
+          },
+          "path": [
+            "color",
+            "colour",
+            "green",
+            "10"
+          ]
+        },
+        "100": {
+          "value": "#007064",
+          "original": {
+            "value": "#007064"
+          },
+          "name": "ColorColourGreen100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "green",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "green",
+            "100"
+          ]
+        }
+      },
+      "interaction": {
+        "focus": {
+          "value": "#0d59ff",
+          "original": {
+            "value": "#0d59ff"
+          },
+          "name": "ColorColourInteractionFocus",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "interaction",
+            "subitem": "focus"
+          },
+          "path": [
+            "color",
+            "colour",
+            "interaction",
+            "focus"
+          ]
+        },
+        "focus-glow": {
+          "value": "#0d59ff",
+          "original": {
+            "value": "rgba(13, 89, 255, 0.2000)"
+          },
+          "name": "ColorColourInteractionFocusGlow",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "interaction",
+            "subitem": "focus-glow"
+          },
+          "path": [
+            "color",
+            "colour",
+            "interaction",
+            "focus-glow"
+          ]
+        }
+      },
+      "invalid": {
+        "10": {
+          "value": "#e60e22",
+          "original": {
+            "value": "rgba(230, 14, 34, 0.1000)"
+          },
+          "name": "ColorColourInvalid10",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "invalid",
+            "subitem": "10"
+          },
+          "path": [
+            "color",
+            "colour",
+            "invalid",
+            "10"
+          ]
+        },
+        "100": {
+          "value": "#e60e22",
+          "original": {
+            "value": "#e60e22"
+          },
+          "name": "ColorColourInvalid100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "invalid",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "invalid",
+            "100"
+          ]
+        }
+      },
+      "neutral": {
+        "0": {
+          "value": "#ffffff",
+          "original": {
+            "value": "#ffffff"
+          },
+          "name": "ColorColourNeutral0",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "0"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "0"
+          ]
+        },
+        "5": {
+          "value": "#000000",
+          "original": {
+            "value": "rgba(0, 0, 0, 0.0500)"
+          },
+          "name": "ColorColourNeutral5",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "5"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "5"
+          ]
+        },
+        "10": {
+          "value": "#000000",
+          "original": {
+            "value": "rgba(0, 0, 0, 0.1000)"
+          },
+          "name": "ColorColourNeutral10",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "10"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "10"
+          ]
+        },
+        "20": {
+          "value": "#cccccc",
+          "original": {
+            "value": "#cccccc"
+          },
+          "name": "ColorColourNeutral20",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "20"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "20"
+          ]
+        },
+        "30": {
+          "value": "#b3b3b3",
+          "original": {
+            "value": "#b3b3b3"
+          },
+          "name": "ColorColourNeutral30",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "30"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "30"
+          ]
+        },
+        "40": {
+          "value": "#999999",
+          "original": {
+            "value": "#999999"
+          },
+          "name": "ColorColourNeutral40",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "40"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "40"
+          ]
+        },
+        "50": {
+          "value": "#7f7f7f",
+          "original": {
+            "value": "#7f7f7f"
+          },
+          "name": "ColorColourNeutral50",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "50"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "50"
+          ]
+        },
+        "60": {
+          "value": "#666666",
+          "original": {
+            "value": "#666666"
+          },
+          "name": "ColorColourNeutral60",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "60"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "60"
+          ]
+        },
+        "70": {
+          "value": "#4d4d4d",
+          "original": {
+            "value": "#4d4d4d"
+          },
+          "name": "ColorColourNeutral70",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "70"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "70"
+          ]
+        },
+        "80": {
+          "value": "#333333",
+          "original": {
+            "value": "#333333"
+          },
+          "name": "ColorColourNeutral80",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "80"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "80"
+          ]
+        },
+        "90": {
+          "value": "#1a1a1a",
+          "original": {
+            "value": "#1a1a1a"
+          },
+          "name": "ColorColourNeutral90",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "90"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "90"
+          ]
+        },
+        "100": {
+          "value": "#000000",
+          "original": {
+            "value": "#000000"
+          },
+          "name": "ColorColourNeutral100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "neutral",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "neutral",
+            "100"
+          ]
+        }
+      },
+      "skeleton": {
+        "dark": {
+          "value": "#e6e6e6",
+          "original": {
+            "value": "#e6e6e6"
+          },
+          "name": "ColorColourSkeletonDark",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "skeleton",
+            "subitem": "dark"
+          },
+          "path": [
+            "color",
+            "colour",
+            "skeleton",
+            "dark"
+          ]
+        }
+      },
+      "yellow": {
+        "100": {
+          "value": "#ffe600",
+          "original": {
+            "value": "#ffe600"
+          },
+          "name": "ColorColourYellow100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "yellow",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "yellow",
+            "100"
+          ]
+        },
+        "600": {
+          "value": "#fff600",
+          "original": {
+            "value": "#fff600"
+          },
+          "name": "ColorColourYellow600",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "yellow",
+            "subitem": "600"
+          },
+          "path": [
+            "color",
+            "colour",
+            "yellow",
+            "600"
+          ]
+        }
+      },
+      "yellow-warm": {
+        "80": {
+          "value": "#ffda00",
+          "original": {
+            "value": "#ffda00"
+          },
+          "name": "ColorColourYellowWarm80",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "yellow-warm",
+            "subitem": "80"
+          },
+          "path": [
+            "color",
+            "colour",
+            "yellow-warm",
+            "80"
+          ]
+        },
+        "100": {
+          "value": "#ffc300",
+          "original": {
+            "value": "#ffc300"
+          },
+          "name": "ColorColourYellowWarm100",
+          "attributes": {
+            "category": "color",
+            "type": "colour",
+            "item": "yellow-warm",
+            "subitem": "100"
+          },
+          "path": [
+            "color",
+            "colour",
+            "yellow-warm",
+            "100"
+          ]
+        }
+      }
+    }
+  }
+};
