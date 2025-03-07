9ff7f2c82b07665560ca5dc5eabf8decf9c4ab84
zeroheight <tokens-management@zeroheight.com>
2025-03-07 14:19:57 +1030

    Update Andrews DS testing from zeroheight

--- js/token_Tokens_Mode1.js
+++ js/token_Tokens_Mode1.js
@@ -1,119 +1,609 @@
-{
+module.exports = {
   "color": {
     "border": {
       "focus": {
-        "value": "{colour.interaction.focus}"
+        "value": "#0d59ff",
+        "original": {
+          "value": "#0d59ff"
+        },
+        "name": "ColorBorderFocus",
+        "attributes": {
+          "category": "color",
+          "type": "border",
+          "item": "focus"
+        },
+        "path": [
+          "color",
+          "border",
+          "focus"
+        ]
       },
       "primary": {
-        "value": "{colour.neutral.100}"
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorBorderPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "border",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "border",
+          "primary"
+        ]
       },
       "secondary-diabled": {
-        "value": "{colour.neutral.20}"
+        "value": "#cccccc",
+        "original": {
+          "value": "#cccccc"
+        },
+        "name": "ColorBorderSecondaryDiabled",
+        "attributes": {
+          "category": "color",
+          "type": "border",
+          "item": "secondary-diabled"
+        },
+        "path": [
+          "color",
+          "border",
+          "secondary-diabled"
+        ]
       },
       "success": {
-        "value": "{colour.green.100}"
+        "value": "#007064",
+        "original": {
+          "value": "#007064"
+        },
+        "name": "ColorBorderSuccess",
+        "attributes": {
+          "category": "color",
+          "type": "border",
+          "item": "success"
+        },
+        "path": [
+          "color",
+          "border",
+          "success"
+        ]
       }
     },
     "button": {
       "invalid": {
-        "value": "{colour.invalid.100}"
+        "value": "#e60e22",
+        "original": {
+          "value": "#e60e22"
+        },
+        "name": "ColorButtonInvalid",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "invalid"
+        },
+        "path": [
+          "color",
+          "button",
+          "invalid"
+        ]
       },
       "primary": {
-        "value": "{colour.yellow.100}"
+        "value": "#ffe600",
+        "original": {
+          "value": "#ffe600"
+        },
+        "name": "ColorButtonPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "button",
+          "primary"
+        ]
       },
       "primary-active": {
-        "value": "{surface.brand-contrast}"
+        "value": "#ffda00",
+        "original": {
+          "value": "#ffda00"
+        },
+        "name": "ColorButtonPrimaryActive",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "primary-active"
+        },
+        "path": [
+          "color",
+          "button",
+          "primary-active"
+        ]
       },
       "primary-disbled": {
-        "value": "{surface.disabled}"
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.1000)"
+        },
+        "name": "ColorButtonPrimaryDisbled",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "primary-disbled"
+        },
+        "path": [
+          "color",
+          "button",
+          "primary-disbled"
+        ]
       },
       "primary-focus": {
-        "value": "{surface.primary}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorButtonPrimaryFocus",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "primary-focus"
+        },
+        "path": [
+          "color",
+          "button",
+          "primary-focus"
+        ]
       },
       "primary-hover": {
-        "value": "{surface.brand-secondary}"
+        "value": "#fff600",
+        "original": {
+          "value": "#fff600"
+        },
+        "name": "ColorButtonPrimaryHover",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "primary-hover"
+        },
+        "path": [
+          "color",
+          "button",
+          "primary-hover"
+        ]
       },
       "secondary": {
-        "value": "{colour.neutral.0}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorButtonSecondary",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "secondary"
+        },
+        "path": [
+          "color",
+          "button",
+          "secondary"
+        ]
       },
       "secondary-active": {
-        "value": "{colour.neutral.10}"
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.1000)"
+        },
+        "name": "ColorButtonSecondaryActive",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "secondary-active"
+        },
+        "path": [
+          "color",
+          "button",
+          "secondary-active"
+        ]
       },
       "secondary-disabled": {
-        "value": "{surface.disabled-secondary}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorButtonSecondaryDisabled",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "secondary-disabled"
+        },
+        "path": [
+          "color",
+          "button",
+          "secondary-disabled"
+        ]
       },
       "secondary-focus": {
-        "value": "{surface.primary}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorButtonSecondaryFocus",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "secondary-focus"
+        },
+        "path": [
+          "color",
+          "button",
+          "secondary-focus"
+        ]
       },
       "secondary-hover": {
-        "value": "{colour.neutral.5}"
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.0500)"
+        },
+        "name": "ColorButtonSecondaryHover",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "secondary-hover"
+        },
+        "path": [
+          "color",
+          "button",
+          "secondary-hover"
+        ]
       },
       "selected": {
-        "value": "{colour.green.100}"
+        "value": "#007064",
+        "original": {
+          "value": "#007064"
+        },
+        "name": "ColorButtonSelected",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "selected"
+        },
+        "path": [
+          "color",
+          "button",
+          "selected"
+        ]
       },
       "skeleton": {
-        "value": "{colour.skeleton.dark}"
+        "value": "#e6e6e6",
+        "original": {
+          "value": "#e6e6e6"
+        },
+        "name": "ColorButtonSkeleton",
+        "attributes": {
+          "category": "color",
+          "type": "button",
+          "item": "skeleton"
+        },
+        "path": [
+          "color",
+          "button",
+          "skeleton"
+        ]
       }
     },
     "icon": {
       "brand": {
-        "value": "{colour.neutral.100}"
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorIconBrand",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "brand"
+        },
+        "path": [
+          "color",
+          "icon",
+          "brand"
+        ]
       },
       "disabled": {
-        "value": "{colour.neutral.40}"
+        "value": "#999999",
+        "original": {
+          "value": "#999999"
+        },
+        "name": "ColorIconDisabled",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "disabled"
+        },
+        "path": [
+          "color",
+          "icon",
+          "disabled"
+        ]
       },
       "invalid": {
-        "value": "{colour.invalid.100}"
+        "value": "#e60e22",
+        "original": {
+          "value": "#e60e22"
+        },
+        "name": "ColorIconInvalid",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "invalid"
+        },
+        "path": [
+          "color",
+          "icon",
+          "invalid"
+        ]
       },
       "invert": {
-        "value": "{colour.neutral.0}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorIconInvert",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "invert"
+        },
+        "path": [
+          "color",
+          "icon",
+          "invert"
+        ]
       },
       "primary": {
-        "value": "{colour.neutral.100}"
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorIconPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "icon",
+          "primary"
+        ]
       },
       "success": {
-        "value": "{colour.green.100}"
+        "value": "#007064",
+        "original": {
+          "value": "#007064"
+        },
+        "name": "ColorIconSuccess",
+        "attributes": {
+          "category": "color",
+          "type": "icon",
+          "item": "success"
+        },
+        "path": [
+          "color",
+          "icon",
+          "success"
+        ]
       }
     },
     "surface": {
       "brand-contrast": {
-        "value": "{colour.yellow-warm.80}"
+        "value": "#ffda00",
+        "original": {
+          "value": "#ffda00"
+        },
+        "name": "ColorSurfaceBrandContrast",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "brand-contrast"
+        },
+        "path": [
+          "color",
+          "surface",
+          "brand-contrast"
+        ]
       },
       "brand-secondary": {
-        "value": "{colour.yellow.600}"
+        "value": "#fff600",
+        "original": {
+          "value": "#fff600"
+        },
+        "name": "ColorSurfaceBrandSecondary",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "brand-secondary"
+        },
+        "path": [
+          "color",
+          "surface",
+          "brand-secondary"
+        ]
       },
       "disabled": {
-        "value": "{colour.neutral.10}"
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.1000)"
+        },
+        "name": "ColorSurfaceDisabled",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "disabled"
+        },
+        "path": [
+          "color",
+          "surface",
+          "disabled"
+        ]
       },
       "disabled-secondary": {
-        "value": "{colour.neutral.0}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorSurfaceDisabledSecondary",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "disabled-secondary"
+        },
+        "path": [
+          "color",
+          "surface",
+          "disabled-secondary"
+        ]
       },
       "invalid": {
-        "value": "{colour.invalid.10}"
+        "value": "#e60e22",
+        "original": {
+          "value": "rgba(230, 14, 34, 0.1000)"
+        },
+        "name": "ColorSurfaceInvalid",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "invalid"
+        },
+        "path": [
+          "color",
+          "surface",
+          "invalid"
+        ]
       },
       "primary": {
-        "value": "{colour.neutral.0}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorSurfacePrimary",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "surface",
+          "primary"
+        ]
       },
       "success": {
-        "value": "{colour.green.10}"
+        "value": "#e6f1f0",
+        "original": {
+          "value": "#e6f1f0"
+        },
+        "name": "ColorSurfaceSuccess",
+        "attributes": {
+          "category": "color",
+          "type": "surface",
+          "item": "success"
+        },
+        "path": [
+          "color",
+          "surface",
+          "success"
+        ]
       }
     },
     "text": {
       "brand": {
-        "value": "{colour.neutral.100}"
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorTextBrand",
+        "attributes": {
+          "category": "color",
+          "type": "text",
+          "item": "brand"
+        },
+        "path": [
+          "color",
+          "text",
+          "brand"
+        ]
       },
       "disbled": {
-        "value": "{colour.neutral.40}"
+        "value": "#999999",
+        "original": {
+          "value": "#999999"
+        },
+        "name": "ColorTextDisbled",
+        "attributes": {
+          "category": "color",
+          "type": "text",
+          "item": "disbled"
+        },
+        "path": [
+          "color",
+          "text",
+          "disbled"
+        ]
       },
       "invert": {
-        "value": "{colour.neutral.0}"
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorTextInvert",
+        "attributes": {
+          "category": "color",
+          "type": "text",
+          "item": "invert"
+        },
+        "path": [
+          "color",
+          "text",
+          "invert"
+        ]
       },
       "primary": {
-        "value": "{colour.neutral.100}"
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorTextPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "text",
+          "item": "primary"
+        },
+        "path": [
+          "color",
+          "text",
+          "primary"
+        ]
       },
       "success": {
-        "value": "{colour.green.100}"
+        "value": "#007064",
+        "original": {
+          "value": "#007064"
+        },
+        "name": "ColorTextSuccess",
+        "attributes": {
+          "category": "color",
+          "type": "text",
+          "item": "success"
+        },
+        "path": [
+          "color",
+          "text",
+          "success"
+        ]
       }
     }
   }
-}
+};
